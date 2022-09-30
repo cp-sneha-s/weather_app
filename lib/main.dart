3df9c12d7486f9a409ea18c814813a882d1086dd
sneha.s <sneha.s@canopas.com>
2022-09-30 12:45:53 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather_app/bloc/weather_bloc.dart';
-import 'package:weather_app/repo/weather_repo.dart';
 import 'package:weather_app/screens/home_screen.dart';
 
 
@@ -15,13 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  BlocProvider(
-      create: (_)=>WeatherBloc(WeatherRepository()),
-      child:  MaterialApp(
-          title: 'Weather app',
-          home:   const HomeScreen()
-      ),
-    );
+    return MaterialApp(title: 'Weather app', home: const HomeScreen());
   }
 }
 
--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather_app/bloc/weather_bloc.dart';
-import 'package:weather_app/bloc/weather_cubit.dart';
-import 'package:weather_app/core/extension.dart';
-import 'package:weather_app/model/weather.dart';
+
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({Key? key}) : super(key: key);
@@ -29,31 +25,20 @@ class _WeatherScreenState extends State<WeatherScreen> {
         appBar: AppBar(
           title: const Text('Weather'),
         ),
-        body: BlocBuilder<WeatherBloc, WeatherState>(
-            builder: (BuildContext context, WeatherState state) {
-          if (state is InitialState) {
-            return Container();
-          } else if (state is LoadingState) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is LoadedState) {
-            WeatherData weatherData = state.weatherData;
-            temp = weatherData.temp!.toInt();
-            condition = weatherData.cod;
-            weatherIcon = condition.getWeatherIcon();
-            weatherMessage = temp.getWeatherMessage();
-
-            return Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
+        body: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
                   children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          '$temp°',
-                          style: const TextStyle(fontSize: 25),
-                        ),
+                    Text(
+                      '',
+             //TODO Replace empty text with '$temp',
+
+                      style: const TextStyle(fontSize: 25),
+                    ),
                         Text(
                           weatherIcon,
                         ),
@@ -63,26 +48,22 @@ class _WeatherScreenState extends State<WeatherScreen> {
                       height: 20,
                     ),
                     Text(
-                      '$weatherMessage in ${weatherData.name}!',
+                      '',
+                      //TODO Replace empty text with '$weatherMessage in ${weatherData.name}!',
+
                       textAlign: TextAlign.right,
                       style: TextStyle(fontSize: 25),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      'Looks like: ${weatherData.description}!',
-                      textAlign: TextAlign.right,
-                      style: const TextStyle(fontSize: 25),
-                    ),
-                  ],
-                ));
-          } else if (state is ErrorState) {
-            String error = state.error;
-            ScaffoldMessenger.of(context)
-                .showSnackBar(SnackBar(content: Text(error)));
-          }
-          return Container();
-        }));
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  '',
+                  //TODO Replace empty text 'Looks like: ${weatherData.description}!',
+                  textAlign: TextAlign.right,
+                  style: const TextStyle(fontSize: 25),
+                ),
+              ],
+            )));
   }
 }
